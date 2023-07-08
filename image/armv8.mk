dc9218d4ec140ea77a5a760a70d916642450d439
Bard L <admin@cooluc.com>
2023-07-08 10:22:31 +0800

    rockchip: nanopi-r5c: remove rtw88 & mt7922 driver

--- image/armv8.mk
+++ image/armv8.mk
@@ -21,7 +21,7 @@ define Device/friendlyarm_nanopi-r5c
   SOC := rk3568
   UBOOT_DEVICE_NAME := nanopi-r5c-rk3568
   IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8125 kmod-rtw88 kmod-mt7921e kmod-mt7922-firmware rtl8822ce-firmware wpad-basic-openssl
+  DEVICE_PACKAGES := kmod-r8125 kmod-mt7921e wpad-basic-openssl
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r5c
 
