895a1db6ffda81d675f9f1863a8456ad4c5d7dad
sbwml <admin@cooluc.com>
2023-10-04 15:34:11 +0800

    nanopi-r5c: drop mt7921 driver

--- image/armv8.mk
+++ image/armv8.mk
@@ -21,7 +21,7 @@ define Device/friendlyarm_nanopi-r5c
   SOC := rk3568
   UBOOT_DEVICE_NAME := nanopi-r5c-rk3568
   IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8125 kmod-mt7921e wpad-basic-openssl
+  DEVICE_PACKAGES := kmod-r8125
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r5c
 
